e1d610491ec44f8df1aa3abffd1e9d0ef3dfc31d
AeternumVoid <luismmendoza.dev@gmail.com>
2026-02-08 21:14:18 -0300

    Add data types example in data_types.js

---
+++ 01-fundamentals/data_types.js
@@ -0,0 +1,8 @@
+// Strings, Numbers, Booleans, Undefined, Null
+let profession = "SDET"; // String
+let yearsToSpain = 3;    // Number
+let isLearningJS = true; // Boolean
+let futureSalary;        // Undefined (automatic)
+const badLuck = null;      // Null (intentional absence)
+
+console.log(typeof profession); // Shows "string"
